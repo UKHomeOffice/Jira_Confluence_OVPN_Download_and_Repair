42717306862288e36e71c4de5f640e7bac1c702c
Ibrahim Mubarik <53750722+Ibby23@users.noreply.github.com>
2021-09-07 13:44:24 +0100

    Add files via upload

---
+++ jira_confluence_vpn_download_repair.ps1
@@ -0,0 +1,12 @@
+﻿$USER = $env:UserName
+$BROWSER_PATH = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+
+Remove-Item -path C:\Users\$USER\Downloads\vpn-jira-confluence*.ovpn
+
+Start-Process -FilePath $BROWSER_PATH "https://access-acp.digital.homeoffice.gov.uk/ui/profiles/jira-confluence/issue?template=Default"
+
+While ((Test-Path -Path C:\Users\$USER\Downloads\vpn-jira-confluence*.ovpn) -eq $false) {
+    Start-Sleep -Seconds 10
+}
+
+Set-Content -Path "C:\Users\$USER\Downloads\vpn-jira-confluence*.ovpn" -Value (Get-Content -Path "C:\Users\$USER\Downloads\vpn-jira-confluence*.ovpn" | Where-Object {$_ -notmatch '\[inline\]'})
