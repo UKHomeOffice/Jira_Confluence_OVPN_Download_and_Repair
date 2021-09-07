370a8d4e84f7705242bb1d1274c028b30490b759
Ibby23 <Mub2305&*()>
2021-09-07 14:42:15 +0100

    Uses the default browser to open the url

--- jira_confluence_vpn_download_repair.ps1
+++ jira_confluence_vpn_download_repair.ps1
@@ -5,7 +5,7 @@ $BROWSER_PATH = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
 
 Remove-Item -path C:\Users\$USER\Downloads\vpn-jira-confluence*.ovpn
 
-Start-Process -FilePath $BROWSER_PATH "https://access-acp.digital.homeoffice.gov.uk/ui/profiles/jira-confluence/issue?template=Default"
+Start-Process "https://access-acp.digital.homeoffice.gov.uk/ui/profiles/jira-confluence/issue?template=Default"
 
 While ((Test-Path -Path C:\Users\$USER\Downloads\vpn-jira-confluence*.ovpn) -eq $false) {
     Start-Sleep -Seconds 10
