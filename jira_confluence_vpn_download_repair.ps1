10612a83a11f7bdd9d00661b9e32fb0b4c4e2aaf
fim17 <fim17@aber.ac.uk>
2021-09-07 15:05:40 +0100

    Adds comments and more consistent syntax

--- jira_confluence_vpn_download_repair.ps1
+++ jira_confluence_vpn_download_repair.ps1
@@ -1,14 +1,16 @@
 ﻿$USER = $env:UserName
 
-#Find the users's default browser and exectuable
-$BROWSER_PATH = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+# removes any jira-confluence ovpn files in users downloads folder
+Remove-Item -Path "C:\Users\$USER\Downloads\vpn-jira-confluence*.ovpn"
 
-Remove-Item -path C:\Users\$USER\Downloads\vpn-jira-confluence*.ovpn
-
+# accesses url for file download using users default browser
 Start-Process "https://access-acp.digital.homeoffice.gov.uk/ui/profiles/jira-confluence/issue?template=Default"
 
-While ((Test-Path -Path C:\Users\$USER\Downloads\vpn-jira-confluence*.ovpn) -eq $false) {
+# stalls the program until new jira-confluence ovpn file has been downloaded
+# should have a termination condition after a certain amount of time but beyond scope of this change
+While ((Test-Path -Path "C:\Users\$USER\Downloads\vpn-jira-confluence*.ovpn") -eq $false) {
     Start-Sleep -Seconds 10
 }
 
+# removes lines in ovpn file causing errors
 Set-Content -Path "C:\Users\$USER\Downloads\vpn-jira-confluence*.ovpn" -Value (Get-Content -Path "C:\Users\$USER\Downloads\vpn-jira-confluence*.ovpn" | Where-Object {$_ -notmatch '\[inline\]'})
